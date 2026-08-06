fd1c8c4039c3cce76ea856bfa3d3bb47a2daa689
Donovan <donovanriano@gmail.com>
2026-08-06 16:31:16 -0600

    chore: traducir categorías del hub y eliminar headerNavLinks muerto

--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/news', title: 'News' },
-  { href: '/projects', title: 'Amaxing info' },
-  { href: '/about', title: 'About' },
-]
-
-export default headerNavLinks
